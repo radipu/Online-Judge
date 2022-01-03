36e16437f13b3556ac9acb78aac910c3cc177529
Rohul Amin Mohammad Dipu <41869272+radipu@users.noreply.github.com>
2022-01-04 00:28:56 +0600

    the 3n+1 problem using .net core 3.1

---
+++ The 3n+1 Problem using-c#.net-core-3.1/The3n+1Problem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace The3n_1Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the value of n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You've entered: " + n);
+            for (int i = n; i > 0;)
+            {
+                Console.WriteLine(n);
+                if (n % 2 == 0)
+                {
+                    n = n / 2;
+                }
+                else
+                {
+                    n = 3 * n + 1;
+                }
+                i = n;
+                if (i == 1)
+                {
+                    Console.WriteLine(n);
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
